6fc2b29723d85470d7b607b6427d3070752dd020
Louis Mukama <louisvitz21@gmail.com>
2026-07-08 10:14:35 +0300

    build and validate flat mart

--- 2_DW_Mart_Build/build_dw_marts.sql
+++ 2_DW_Mart_Build/build_dw_marts.sql
@@ -1,6 +1,13 @@
+--duckdb dw_marts.duckdb -c ".read build_dw_marts.sql"
+
+
 
 -- step 1:  DW -Create star scheme tables
 .read 01_create_tables_dw.sql
 
 -- step 2: DW -load data from csv files into tables 
 .read 02_load_schema_dw.sql
+
+
+-- step 3: Mart - Create flat mart
+.read 03_create_flat_mart.sql
